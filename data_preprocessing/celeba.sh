9a874cf1702508554f92507d542daf3c08ba16df
Tianyu Wang <1463026393@qq.com>
2020-02-20 17:04:54 +0000

    mask validation set

--- data_preprocessing/celeba.sh
+++ data_preprocessing/celeba.sh
@@ -1,5 +1,6 @@
 mkdir datasets
-python ./scripts/flist.py --path ../edge_connect/img_align_celeba/img_align_celeba --output ./datasets/celeba_train.flist
-python ./scripts/flist.py --path ../edge_connect/data/celeba/test --output ./datasets/celeba_test.flist
-python ./scripts/flist.py --path ../edge_connect/data/celeba/eval --output ./datasets/celeba_eval.flist
-python ./scripts/flist.py --path ../edge_connect/mask/testing_mask_dataset --output ./datasets/mask_test.flist
+python ./scripts/flist.py --path ../edge-connect/img_align_celeba/img_align_celeba --output ./datasets/celeba_train.flist
+python ./scripts/flist.py --path ../edge-connect/data/celeba/test --output ./datasets/celeba_test.flist
+python ./scripts/flist.py --path ../edge-connect/data/celeba/eval --output ./datasets/celeba_eval.flist
+python ./scripts/flist.py --path ../edge-connect/mask/testing_mask_dataset --output ./datasets/mask_test.flist
+python ./data_preprocessing/create_mask_test.py --path ../edge-connect/mask/testing_mask_dataset --output ./datasets/mask_test_same_size.flist
---
+++ data_preprocessing/create_mask_test.py
@@ -0,0 +1,34 @@
+import os
+import argparse
+import numpy as np
+from random import random
+from random import seed
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--path', type=str, help='path to the dataset')
+parser.add_argument('--output', type=str, help='path to the file list')
+args = parser.parse_args()
+
+test_set_num = 0
+
+with open('list_eval_partition.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        if row['partition'] == '3':
+            test_set_num += 1
+
+ext = {'.JPG', '.JPEG', '.PNG', '.TIF', 'TIFF'}
+
+images = []
+for root, dirs, files in os.walk(args.path):
+    print('loading ' + root)
+    for file in files:
+        if os.path.splitext(file)[1].upper() in ext:
+            images.append(os.path.join(root, file))
+
+seed(21312)
+selected_images = []
+for i in range(0, test_set_num):
+    selected_images.append(images[int(random * 12000)])
+print(test_set_num)
+np.savetxt(args.output, selected_images, fmt='%s')
